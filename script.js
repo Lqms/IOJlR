5c581574b7886edea5b9eceebd0ddc644f510298
Luqmus <42968850+Lqms@users.noreply.github.com>
2023-08-29 19:30:03 +0400

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function nextSlide() {
 
 // смена внутренностей слайда
  function change_inner(){
-     heading_one.innerHTML = `Если бы Карина была ${titles[slide]}, то это определённо:`;
+     heading_one.innerHTML = `Если бы Вика была ${titles[slide]}, то это определённо:`;
      heading_two.innerHTML = `${title_names[slide]}`;
      paragraph.innerHTML = `Потому что ${title_info[slide]}`;
      image.style.backgroundImage = `url(image${slide}.jpg)` 
